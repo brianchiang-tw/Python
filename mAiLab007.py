5a416153874b0ecf6b2ab0f23f4f2de181ef31f2
Brian Chiang <brianchiang1988@icloud.com>
2019-06-22 18:11:25 +0800

    complete mAiLab007

---
+++ mAiLab007.py
@@ -0,0 +1,19 @@
+# Objective of mAiLab001, in this self-learning training course, is as following: 
+
+# 1. Study radial basis function (i.e., RBF [1]) and understand its mathematical meaning as well as application on machine learning.
+
+# 2. Preview algorithm of LeNet5 [2] and get ready into implementation.
+
+
+
+
+# Reference:
+
+# [1]. Radial_basis_function
+# https://en.wikipedia.org/wiki/Radial_basis_function
+# https://www.cs.cmu.edu/afs/cs/academic/class/15883-f13/slides/rbf.pdf
+
+
+
+# [2]. Gradient-Based Learning Applied to Document Recognition
+# http://yann.lecun.com/exdb/publis/pdf/lecun-01a.pdf
